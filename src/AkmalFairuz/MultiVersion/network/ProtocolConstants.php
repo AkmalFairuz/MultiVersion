f0ed8c5917feabe2d1fae0610fdb3d528c51ccd0
kanison konkla <53283017+pooooooon@users.noreply.github.com>
2021-11-01 10:37:28 +0700

    beta protocol

--- src/AkmalFairuz/MultiVersion/network/ProtocolConstants.php
+++ src/AkmalFairuz/MultiVersion/network/ProtocolConstants.php
@@ -8,11 +8,26 @@ use pocketmine\network\mcpe\protocol\ProtocolInfo;
 
 class ProtocolConstants{
 
+    /* Support*/
+    public const BEDROCK_1_16_220_50 = 429;
+    public const BEDROCK_1_16_220_51 = 430;
     public const BEDROCK_1_16_220 = 431;
     public const BEDROCK_1_17_0 = 440;
+    public const BEDROCK_1_17_10_20 = 441;
     public const BEDROCK_1_17_10 = 448;
+    public const BEDROCK_1_17_20_20 = 453;
+    public const BEDROCK_1_17_20_21 = 455;
+    public const BEDROCK_1_17_20_22 = 456;
+    public const BEDROCK_1_17_20_23 = 459;
+    public const BEDROCK_1_17_30_20 = 462;
+    public const BEDROCK_1_17_30_22 = 464;
     public const BEDROCK_1_17_30 = 465;
-
+    /* Support Block is Weird (No Pallet)*/
+    public const BEDROCK_1_16_230_50 = 433;
+    public const BEDROCK_1_16_230_52 = 434;
+    public const BEDROCK_1_16_230_54 = 435;
+    public const BEDROCK_1_17_0_50 = 437;
+    
     public const SUPPORTED_PROTOCOLS = [
         self::BEDROCK_1_16_220,
         self::BEDROCK_1_17_0,
