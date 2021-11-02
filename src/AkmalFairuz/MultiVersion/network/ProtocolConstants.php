36038a6157cc5d49607910da2a43351c8a70d1ef
kanison konkla <53283017+pooooooon@users.noreply.github.com>
2021-11-02 09:38:51 +0700

    now support

--- src/AkmalFairuz/MultiVersion/network/ProtocolConstants.php
+++ src/AkmalFairuz/MultiVersion/network/ProtocolConstants.php
@@ -12,6 +12,9 @@ class ProtocolConstants{
     public const BEDROCK_1_16_220_50 = 429;
     public const BEDROCK_1_16_220_51 = 430;
     public const BEDROCK_1_16_220 = 431;
+    public const BEDROCK_1_16_230_50 = 433;
+    public const BEDROCK_1_16_230_52 = 434;
+    public const BEDROCK_1_16_230_54 = 435;
     public const BEDROCK_1_17_0 = 440;
     public const BEDROCK_1_17_10_20 = 441;
     public const BEDROCK_1_17_10 = 448;
@@ -22,16 +25,25 @@ class ProtocolConstants{
     public const BEDROCK_1_17_30_20 = 462;
     public const BEDROCK_1_17_30_22 = 464;
     public const BEDROCK_1_17_30 = 465;
-    /* Support Block is Weird (No Block States)*/
-    public const BEDROCK_1_16_230_50 = 433;
-    public const BEDROCK_1_16_230_52 = 434;
-    public const BEDROCK_1_16_230_54 = 435;
+    /* Support but Block is Weird (No Block States)*/
     public const BEDROCK_1_17_0_50 = 437;
     
     public const SUPPORTED_PROTOCOLS = [
+        self::BEDROCK_1_16_220_50,
+        self::BEDROCK_1_16_220_51,
         self::BEDROCK_1_16_220,
+        self::BEDROCK_1_16_230_50,
+        self::BEDROCK_1_16_230_52,
+        self::BEDROCK_1_16_230_54,
         self::BEDROCK_1_17_0,
+        self::BEDROCK_1_17_10_20,
         self::BEDROCK_1_17_10,
+        self::BEDROCK_1_17_20_20,
+        self::BEDROCK_1_17_20_21,
+        self::BEDROCK_1_17_20_22,
+        self::BEDROCK_1_17_20_23,
+        self::BEDROCK_1_17_30_20,
+        self::BEDROCK_1_17_30_22,
         self::BEDROCK_1_17_30,
         ProtocolInfo::CURRENT_PROTOCOL
     ];
